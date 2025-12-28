b033ab2809d0ff5109fe51a9f6c4c7a2343f6f43
jasoncross <jason@jasoncross.com>
2025-12-28 15:54:13 -0600

    Add limelight last-known-good fallback

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/LimelightPipelineAutoSelector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/LimelightPipelineAutoSelector.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.vision;
 import android.content.Context;
 import android.content.SharedPreferences;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.util.AppUtil;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 import org.firstinspires.ftc.teamcode.Alliance;
 import org.firstinspires.ftc.teamcode.config.LimelightPipelineAutoSelectConfig;
 import org.firstinspires.ftc.teamcode.config.VisionConfig;
